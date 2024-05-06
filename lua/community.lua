630cf4fad5a3305d4a8d1e12dc509a2672b88871
David Salter <davidmsalter@me.com>
2024-05-06 21:10:48 +0100

    Initial commit

---
+++ lua/community.lua
@@ -0,0 +1,29 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.java" },
+  {
+    "mfussenegger/nvim-jdtls",
+    opts = {
+      settings = {
+        java = {
+          configuration = {
+            runtimes = {
+              {
+                name = "JavaSE-1.8",
+                path = "/home/david/.sdkman/candidates/java/8.0.412-zulu",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+
+  -- import/override with your plugins folder
+}
